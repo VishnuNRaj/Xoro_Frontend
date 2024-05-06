b9aebd3571aa656d696a335e02e7f0e59a9f1893
Vishnu N Raj <vishnu8240.achu@gmail.com>
2024-05-06 17:42:10 +0530

    06-05-24 17.42

--- src/Assets/Components/Google.tsx
+++ src/Assets/Components/Google.tsx
@@ -2,36 +2,38 @@ import React from 'react';
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { googleConfig } from '../../Configs/googleConfig'
 interface props {
     socialMedia: Function;
 }
 const Google: React.FC<props> = ({ socialMedia }) => {
+    const { CliendID } = googleConfig
     const responseMessage = (response: CredentialResponse) => {
         console.log('Login successful:', response);
         if (response.credential) {
-            const userData:{
-                email:string;
-                name:string;
-                sub:string | number;
-                picture:string;
+            const userData: {
+                email: string;
+                name: string;
+                sub: string | number;
+                picture: string;
             } = jwtDecode(response.credential);
             console.log(userData)
-            if(userData) {
+            if (userData) {
                 socialMedia({
-                    type:'Google',
+                    type: 'Google',
                     user: {
                         Email: userData.email,
                         Name: userData.name,
-                        Password:userData.sub,
-                        Profile:userData.picture
+                        Password: userData.sub,
+                        Profile: userData.picture
                     }
                 })
             }
         }
     };
-
+    console.log(googleConfig)
     return (
-        <GoogleOAuthProvider clientId={'720396660551-d9a75lb492chtafthduq03ojbugu2qn2.apps.googleusercontent.com'}>
+        <GoogleOAuthProvider clientId={CliendID}>
             <div>
                 <GoogleLogin
                     onSuccess={responseMessage}
--- src/Configs/googleConfig.ts
+++ src/Configs/googleConfig.ts
@@ -1,9 +1,8 @@
 import { googleConfigInterface } from '../Interfaces/UserInterface'
-export const googleConfig = ():googleConfigInterface => {
-    console.log(import.meta.env)
-    return <googleConfigInterface>{
-        CliendID: import.meta.env.VITE_GOOGLE_CLIEND_ID || '',
-        Secret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET || '',
-        RedirectURL: import.meta.env.VITE_APP_REDIRECT_URL || '',
-    }
+console.log(import.meta.env)
+
+export const googleConfig = <googleConfigInterface>{
+    CliendID: import.meta.env.VITE_GOOGLE_CLIENT_ID || '',
+    Secret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET || '',
+    RedirectURL: import.meta.env.VITE_APP_REDIRECT_URL || '',
 }
