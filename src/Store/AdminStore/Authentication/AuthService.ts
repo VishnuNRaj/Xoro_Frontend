0028ac301650ddb63f16a643fb5ac26b4b550411
Vishnu N Raj <vishnu8240.achu@gmail.com>
2024-07-27 20:47:08 +0530

    fixed admin 6

--- src/Store/AdminStore/Authentication/AuthService.ts
+++ src/Store/AdminStore/Authentication/AuthService.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 import * as interfaces from "./Interfaces";
 import config from "../../../Configs/config";
 // import { getCookie } from "../../../Functions/CommonFunctions";
-axios.interceptors.request.use(config=>{
-    config.withCredentials = true;
-    // const token = getCookie("admin"); 
-    // if (token) {
-    //     config.headers.Authorization = `${token}`;
-    // }
-    return config;
-})
+// axios.interceptors.request.use(config=>{
+//     config.withCredentials = true;
+//     // const token = getCookie("admin"); 
+//     // if (token) {
+//     //     config.headers.Authorization = `${token}`;
+//     // }
+//     return config;
+// })
 export const adminLogin: Function = async (data: interfaces.AdminLogin): Promise<interfaces.AdminLoginResponse> => {
     try {
         const response = await axios.post(`${config.ADMIN}/login`, data, {
@@ -55,7 +55,7 @@ export const verifyAdmin: Function = async (data: interfaces.adminVerifyAuth): P
     try {
         const response = await axios.get(`${config.ADMIN}/verify`, {
             headers: {
-                Authorization: `${data.token}`
+                Authorization: data.token
             }
         })
         return <interfaces.adminVerifyAuthResponse>response.data
