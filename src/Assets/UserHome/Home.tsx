ef38088355f56818d28484eb1e68e94ac3f15335
Vishnu N Raj <vishnu8240.achu@gmail.com>
2024-08-05 09:09:05 +0530

    commited

--- src/Assets/UserHome/Home.tsx
+++ src/Assets/UserHome/Home.tsx
@@ -5,12 +5,12 @@ import useHooks from './Hooks';
 import PostShowComponent from './PostShowComponent';
 // import Recommendations from './Recommendations';
 // import { useOnline } from '../../Other/Hooks';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import ChatLoader from '../Chat/ChatLoader';
+// import InfiniteScroll from 'react-infinite-scroll-component';
+// import ChatLoader from '../Chat/ChatLoader';
 
 const Home: React.FC = () => {
     const { auth } = useEssentials()
-    const { post, skipping, noMore } = useHooks()
+    const { post, noMore } = useHooks()
     const { loading } = auth;
     // const { online } = useOnline()
     // console.log(online, "______")
@@ -22,11 +22,11 @@ const Home: React.FC = () => {
                 <div className='w-full flex p-1 md:pl-20'>
                     <div className='grid grid-cols-1 space-y-8 w-full md:w-2/5 h-auto'>
                         {!noMore ? (
-                            // <InfiniteScroll hasMore={noMore} next={skipping} loader={<ChatLoader />} dataLength={post.length} >
+                            <>
                                 {post && post.length > 0 && post?.map((item) => (
                                     <div key={item._id} ><PostShowComponent postData={item} /></div>
                                 ))}
-                            {/* </InfiniteScroll> */}
+                            </>
                         ) : (
                             <>{post && post.length > 0 && post?.map((item) => (
                                 <div key={item._id} ><PostShowComponent postData={item} /></div>
